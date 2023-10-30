dd0b6579790a700a44f8856d0a60307817d269a3
Netanel Friedman <netanel.friedman6@gmail.com>
2023-10-30 18:23:37 +0200

    Start of rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,10 @@
 use macroquad::prelude::*;
-use macroquad::rand::ChooseRandom;
-
-mod draw;
-use draw::Drawable;
-
-mod game;
-use game::Game;
-
-mod bot;
-use bot::get_best_moves;
-
-mod constants;
-mod grid;
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let mut game = Game::new();
     loop {
-        let bounds = calc_screen_rect();
-        if is_mouse_button_pressed(MouseButton::Left) && game.mouse_press(bounds) {
-            let bot_suggestions = get_best_moves(&mut game);
-            if let Some(bot_best_move) = bot_suggestions.moves.choose().cloned() {
-                game.play(bot_best_move.into_iter());
-            }
-            game.new_suggestion();
-        }
+        clear_background(BLACK);
 
-        if is_key_pressed(KeyCode::R) {
-            game = Game::new();
-            game.new_suggestion();
-        }
-
-        if is_key_pressed(KeyCode::Z) {
-            game.rewind_step();
-            game.new_suggestion();
-        }
-
-        if is_key_pressed(KeyCode::S) {
-            game.suggest = !game.suggest;
-            game.new_suggestion();
-        }
-
-        game.draw(bounds, true);
         next_frame().await
     }
 }
@@ -55,13 +18,4 @@ fn window_conf() -> Conf {
         window_resizable: true,
         ..Default::default()
     }
-}
-
-fn calc_screen_rect() -> Rect {
-    let w = screen_width();
-    let h = screen_height();
-    let size = w.min(h);
-    let x = (w - size) / 2.;
-    let y = (h - size) / 2.;
-    Rect::new(x, y, size, size)
 }
